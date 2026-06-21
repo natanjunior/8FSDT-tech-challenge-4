eb4b5ae3a1b6441038c1753dd19f793b1ef2b84f
Natan <natan.dsjr@gmail.com>
2026-06-21 13:55:24 -0300

    fix(test): update Jest config to work with react-native preset and Zod validation schemas

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'jest-expo',
+  preset: '@react-native/jest-preset',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
@@ -7,7 +7,11 @@ module.exports = {
     '^expo-linear-gradient$': '<rootDir>/__mocks__/expo-linear-gradient/index.tsx',
     '^expo-blur$': '<rootDir>/__mocks__/expo-blur/index.tsx',
   },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['babel-jest', { presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'] }],
+  },
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|expo-asset|expo-font|expo-modules-core|react-clone-referenced-element|@react-navigation|@react-native-async-storage|nativewind|react-native-css-interop))',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|expo-asset|expo-font|expo-modules-core|react-clone-referenced-element|@react-navigation|@react-native-async-storage|nativewind|react-native-css-interop|zod))',
   ],
 };
